69c084dc24c8a6db60f0b4d4cbefea3d15f13b46
Alberto <alberto-bottarini@users.noreply.github.com>
2020-10-09 11:25:10 +0200

    Remove useless assets

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -15,10 +15,7 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-image-with-thumbs', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-image-with-thumbs', __DIR__.'/../dist/css/field.css');
-        });
+        //
     }
 
     /**
